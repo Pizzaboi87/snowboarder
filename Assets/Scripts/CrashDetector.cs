4834fb2990b52fecf0bcc212d1c308e36253960a
Peter Weiser <peterweiser87@gmail.com>
2023-04-30 17:08:45 +0300

    level restart added

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
 
     void OnCollisionEnter2D(Collision2D other)
     {
-        Debug.Log("You bumped your head.");
+        SceneManager.LoadScene(0);
     }
 }
